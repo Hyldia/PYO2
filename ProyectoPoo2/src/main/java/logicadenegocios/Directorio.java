7d5efcd78f886ee340bc3b4acd2880634c7097fe
Berenice Amador Pereira <125695951+bamador101@users.noreply.github.com>
2024-11-17 19:42:52 -0600

    metodos para consultar informacion

--- ProyectoPoo2/src/main/java/logicadenegocios/Directorio.java
+++ ProyectoPoo2/src/main/java/logicadenegocios/Directorio.java
@@ -51,4 +51,8 @@ public class Directorio {
     return fechaCreacion;
   }
 
+    public String getAbsolutePath() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
